01d1d40b91a353746e5c2dbed370711657808fd8
Ayush saxena <2004.ayush01@gmail.com>
2026-03-12 21:59:19 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,6 @@
+import java.util.*;
+public class test {
+    public static void main(String[] args){ // THIS VOID MAIN is function that is used to perform any task
+        System.out.println("Hello Test");
+    }
+}
---
+++ var_1.java
@@ -0,0 +1,30 @@
+public class var_1 {
+    public static void main(String [] args){
+       /*  // variables
+       String name = "Ayush saxena";
+       int a  = 89;
+       int b = 82;
+       int age = 22;
+       double price = 25.25;
+       
+       b = 20; // can be changed;
+       */
+       
+       /* 
+       int a = 10;
+       int b = 25;
+       int sum = a+b;
+       System.out.print(sum);
+       */ 
+
+       int a = 10;
+       int b = 25;
+       int sum = a+b;
+       //System.out.print(sum);
+       int mul = a * b;
+       System.out.println(mul);
+
+
+
+    }
+}
