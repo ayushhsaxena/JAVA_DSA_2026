26060ef118f5183adef4408e146a957f67280110
Ayush saxena <2004.ayush01@gmail.com>
2026-03-14 22:09:17 +0530

    Refactor test class to accept user input
    Updated the test class to include user input for name, age, and remarks.

--- test.java
+++ test.java
@@ -1,6 +1,28 @@
+/* 
 import java.util.*;
 public class test {
     public static void main(String[] args){ // THIS VOID MAIN is function that is used to perform any task
         System.out.println("Hello Test");
+    }
+}
+*/
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter a remark: ");
+        String remarks = sc.nextLine();
+
+        System.out.println(name + " is " + age + " years old and remark is " + remarks);
     }
 }
