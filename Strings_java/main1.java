5b79afa5785fece1e43b7123854a01d2cbd4f2a4
Ayush saxena <2004.ayush01@gmail.com>
2026-04-11 21:39:44 +0530

    Update main1.java

--- Strings_java/main1.java
+++ Strings_java/main1.java
@@ -1,1 +1,15 @@
 
+import java.util.*;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Ayush";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+
+        System.out.println("Reversed: " + rev);
+    }
+}
